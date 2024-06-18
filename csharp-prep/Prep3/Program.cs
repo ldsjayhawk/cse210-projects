33c7e00bd272ae710885dd0b4b9a378b1ae0d897
ldsjayhawk <crisbecca@gmail.com>
2024-06-18 00:09:11 -0500

    add Prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        //Initialize variables
+        int numberGuess = 0;
+        int guess = 0;
+     
+        //Generate a random number between 1 and 100
+        //Random randomNumber = new Random();
+        //int magicNumber = randomNumber.Next(1, 101);
+
+        int magicNumber = 51;
+        
+        while (guess != magicNumber)
+        {
+            numberGuess++;
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+        
+            if (guess > magicNumber)
+            {
+                Console.WriteLine ($"Lower - Your guess was {guess}. Guess #{numberGuess}");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine ($"Higher - Your guess was {guess}. Guess #{numberGuess}");
+            }
+
+            else
+            {
+                Console.Write ($"You guessed it in {numberGuess} tries!");
+            }
+        }
     }
 }
