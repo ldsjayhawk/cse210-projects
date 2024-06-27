ef3515d20486dfc1cd41b3d28e9e0126d68d9645
ldsjayhawk <crisbecca@gmail.com>
2024-06-27 01:54:41 -0500

    beginning wk 3 ancapsulation

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,16 @@
+using System.Data;
+
+public class Scripture
+{
+    public List<string> verse = new List<string>();
+
+    public bool IsCompletelyHidden();
+
+    HideRandomWords();
+
+    GetDisplayText();
+        // Loop through all words and display
+
+// if scripture is completely hidden is performed in program class
+
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,20 @@
+using System.Runtime.CompilerServices;
+
+public class Word
+{
+    public Word(string _text)
+    {
+        private string _text;
+        private bool _isHidden;
+        public string _word; 
+    }
+    Public Word(string text)
+    {
+
+    }
+
+    IsHidden() //getter function returns boolean
+
+    GetDisplayText()
+
+}
