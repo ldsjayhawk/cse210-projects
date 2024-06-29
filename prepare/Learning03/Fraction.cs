7a948f04be6b68bbd431e60c5d9b9852956e712c
ldsjayhawk <crisbecca@gmail.com>
2024-06-29 17:14:32 -0500

    Complete Learining 3

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -5,6 +5,24 @@ public class Fraction
     private int _top;
     private int _bottom;
 
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int number)
+    {
+        _top = number;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
 public int GetTop()
 {
     return _top;
@@ -26,15 +44,13 @@ public void SetBottom(int bottom)
 }
 
 public string GetFractionString()
-{
-    string fraction = _top + " / " + _bottom;
+{   
+    string fraction = $"{_top}/{_bottom}";
     return fraction;
 }
 
 public double GetDecimalValue()
 {
-    double decimalValue = (_top / _bottom);
-    decimalValue = Math.Round(decimalValue);
-    return decimalValue;
+    return (double) _top/ (double) _bottom;
 }
 }
